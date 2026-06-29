205cb6be3bff81cff304e88bf8bb734dc5fb1f02
Marek Rost <marek.rost@gmail.com>
2026-06-29 15:48:05 +0200

    refactor: delegated version management to uv.

--- src/keepassxc_proxy_client/__init__.py
+++ src/keepassxc_proxy_client/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.1.7"
+from importlib.metadata import PackageNotFoundError, version as _pkg_version
+
+try:
+    __version__ = _pkg_version("keepassxc-proxy-client")
+except PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
